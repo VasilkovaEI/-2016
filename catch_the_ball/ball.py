48b6e55f3738a38172009cf2c5dcfab309e8059e
VasilkovaEI <Vasilechek2007@yandex.ru>
2016-07-26 19:31:54 +0300

    Пробы работы с Tkinter.button.

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -17,15 +17,34 @@ def print_hello(event):
         print('You pressed button2!')
     else:
         raise ValueError()
-root = tkinter.Tk()
-
-button1 = tkinter.Button(root, text="Button 1", command=button1_command)
-button1.bind("<Button>",print_hello)
-button1.pack()
-
-button2 = tkinter.Button(root, text="Button 2")
-button2.bind("<Button>",print_hello)
-button2.pack()
 
 
-root.mainloop()
+def inint_main_window():
+    """
+    Инициализация главного окна: создание всех необходимых виджетов и их упаковка.
+    :return:
+    """
+    global root, button1, button2, label, text,scale
+
+    root = tkinter.Tk()
+
+    button1 = tkinter.Button(root, text="Button 1", command=button1_command)
+    button1.bind("<Button>",print_hello)
+
+
+    button2 = tkinter.Button(root, text="Button 2")
+    button2.bind("<Button>",print_hello)
+
+    varible = tkinter.IntVar()
+    label = tkinter.Label(root, text='Some text.')
+    scale = tkinter.Scale(root)
+    text = tkinter.Entry(root)
+
+    for obj in button1, button2, label, scale, text:
+      obj.pack()
+
+
+if __name__== "_main__":
+    init_main_window()
+
+    root.mainloop()
