54e1be22c517874ab52868279f511f216f73c8f2
VasilkovaEI <Vasilechek2007@yandex.ru>
2016-07-27 21:54:30 +0300

    Заготовка игры "Поймай шарик"

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -1,27 +1,65 @@
-
-
 import tkinter
+from random import choice, randint
+
+ball_initial_number = 100
+ball_minimal_radius = 15
+ball_maximal_radius = 40
+ball_available_colors = ['green', 'blue', 'red', 'lightgrey','#FF00FF','#FFFF00']
 
 def click_ball(event):
     """ Обработчик событий мышки  для  игрового холста canvas.
     :param event: событие с координатами клика
-    :return: ничего
+    По клинку мышуой удалять тот объект, на который мышка указывает.
+    А также засчитывает его в очки пользователя.
     """
+    obj = canvas.find_closest(event.x, event.y)
+    x1, y1, x2, y2 = canvas.coords(obj)
+    if x1<=event.x<=x2 and y1<=event.y<=y2:
+        canvas.delete(obj)
+        # FIXME: нужно учесть объект в очках
+        create_random_ball()
 
-    # (event.x, event.y)
 
+def move_all_balls(event):
+    """Перемещает шарики на чуть-чуть"""
+    for obj in canvas.find_all():
+        dx = randint(-1,1)
+        dy = randint(-1,1)
+        canvas.move(obj, dx, dy)
 
 def create_random_ball():
-    canvas.create_oval(x, y, x+2*R, y+2*R, i*40+30, width=1, fill=random_color())
-root = tkinter.Tk()
+    """
+    Создает шарик в случайном месте игрового холста canvas,
+    при этом шарик не выходит за границы холста!
+    """
+    R= randint(ball_minimal_radius, ball_maximal_radius)
+    x= randint(0,int(canvas ['width'])-1-2*R)
+    y= randint(0,int(canvas ['height'])-1-2*R)
+    canvas.create_oval(x, y, x+2*R, y+2*R, width=1, fill=random_color())
 
+def random_color():
+    """
+    :return: Случайный цвет из некоторого набора цветов.
+    """
+    return choice (ball_available_colors)
 
-canvas = tkinter.Canvas(root, background='white', width=400, height=400)
-canvas.bind("<Motion>", paint)
-canvas.pack()
+def init_ball_catch_game():
+    """ Создаем необходимое для игры количество шариков, по которым нужно будет кликать.
+    """
+    for i in range(ball_initial_number):
+        create_random_ball()
 
-for i in range(10):
+def init_main_window():
+    global root, canvas
 
+    root = tkinter.Tk()
+    canvas = tkinter.Canvas(root, background='white', width=400, height=400)
+    canvas.bind("<Button>", click_ball)
+    canvas.bind("<Motion>",move_all_balls)
+    canvas.pack()
 
-root.mainloop()
-print("Эта строка будет достигнута только при выходе из приложения.")
+if __name__== "__main__":
+    init_main_window()
+    init_ball_catch_game()
+    root.mainloop()
+    print("Приходите играть еще!")
